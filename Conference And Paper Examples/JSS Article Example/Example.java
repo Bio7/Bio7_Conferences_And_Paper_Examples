7f1f128cc96b18013da589956e2d24ff3490233a
bio7 <marcel.au@web.de>
2020-12-20 18:11:10 +0100

    Update Commit

---
+++ Conference And Paper Examples/JSS Article Example/Example.java
@@ -0,0 +1,48 @@
+import static com.eco.bio7.spatial.SpatialUtil.*;
+
+/*Java container (arrays) for the data created with R!*/
+int[] x; 
+int[] y; 
+int[] z;
+
+/*The main method with the OpenGL context can be triggered
+  in different time intervals!*/
+public void ecomain(GL gl, GLU glu, GLUT glut) {
+/*After a specified time intervall the data is created!*/
+if (canStep()) {
+/*Method to dynamically create the data in R!*/
+	createRData(); 	
+}        
+/*Rendering of the data with OpenGL!*/ 	
+if (x != null && y != null && z != null) {
+	for (int i = 0; i < z.length; i++) { 			
+	/*OpenGL commands to render the data as primitive points!*/
+		gl.glPushMatrix(); 			
+		gl.glPointSize(1); 			
+		gl.glDisable(GL.GL_LIGHTING); 			
+		gl.glColor4f(1.0f, 1.0f, 1.0f, 1.0f); 			
+		gl.glBegin(GL.GL_POINTS); 			
+		gl.glVertex3i(x[i], y[i], z[i]); 			
+		gl.glEnd(); 			
+		gl.glEnable(GL.GL_LIGHTING); 			
+		gl.glPopMatrix();
+		}
+	}
+}
+public void createRData(){
+RConnection c=RServe.getConnection();
+if (c!=null) {	
+	try { 			
+	     /*Creation of random data in R and the transfer to the Java arrays!*/
+			x = c.eval("rnorm(100000)*100").asIntegers(); 			
+			y = c.eval("rnorm(100000)*100").asIntegers(); 			
+			z = c.eval("rnorm(100000)*100").asIntegers();  			
+		} 
+		catch (RserveException rse) {
+			/*error!*/								
+		} 				
+		catch (REXPMismatchException mme) {
+		 /*error!*/
+		}
+	} 
+}
